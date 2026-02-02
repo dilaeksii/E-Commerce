6c33a9e65c22a4e297c27953a269557ea3066368
dilaeksii <dilaeksii@gmail.com>
2026-02-02 15:17:58 +0300

    shop cards img fix

--- src/data/LinkedCard.js
+++ src/data/LinkedCard.js
@@ -1,22 +1,22 @@
 export const linkedCard = [
     {
-        img: "public/images/shopcard1.jpg",
+        img: "/images/shopcard1.jpg",
         link: "Clothes"
     },
     {
-        img: "public/images/shopcard2.jpg",
+        img: "/images/shopcard2.jpg",
         link: "Sport"
     },
     {
-        img: "public/images/shopcard3.jpg",
+        img: "/images/shopcard3.jpg",
         link: "Accessories"
     },
     {
-        img: "public/images/shopcard4.jpg",
+        img: "/images/shopcard4.jpg",
         link: "Make Up"
     },
     {
-        img: "public/images/shopcard5.jpg",
+        img: "/images/shopcard5.jpg",
         link: "Jackets"
     },
 ]
